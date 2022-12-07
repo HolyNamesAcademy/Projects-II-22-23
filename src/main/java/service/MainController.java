26dafced46b682147fd8bba25eb4e985e11b5169
anaiysaa <anaiysaadeswal@outlook.com>
2022-12-07 07:41:18 -0800

    11/16/22

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @Controller
 public class MainController {
@@ -21,18 +17,29 @@ public class MainController {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello";
 
 
 	}
-	@PostMapping("/api")
-	public String index() {
-		return "index";
+
+
+	@GetMapping("/api")
+	public String callAPI() {
+		plantAPi.api();
+		return "Hello";
 	}
 
-	@PostMapping("/api")
+	/* function httpGet(Url)
+	{
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", https://house-plants.p.rapidapi.com/common/coralberry, false ); // false for synchronous request
+		xmlHttp.send( https://house-plants.p.rapidapi.com/common/coralberry );
+		return xmlHttp.responseText;
+	}
 	HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 	public respHttpResponse<String> getResponse() {return response;}
+	 */
+
 }
