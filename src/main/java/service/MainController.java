fda6aaa6c270b982f87987a5d243b1d29182de47
AerialAstrea <elizabethtdau@gmail.com>
2023-02-15 11:12:21 -0800

    :))

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 @Controller
 public class MainController {
@@ -23,9 +23,34 @@ public class MainController {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+
 		return "hello";
+	}
 
+	@GetMapping ("/uploadPage")
+	public String uploadPage(Model m){
+		Post post = new Post(); // needs arguments but if i keep the default then the upload success page doesn't work
+		m.addAttribute("plant");
 
+		List<String> listStatus = Arrays.asList("Alive", "Dead", "Watered", "Un-Watered");
+		m.addAttribute("post", post);
+		m.addAttribute("listStatus", listStatus);
+
+		return "uploadPage";
+	}
+	
+	@PostMapping("/uploadPage")
+	public String submitUploadPage(@ModelAttribute("post") Post post, Model m){
+		m.addAttribute("Age", post.getAge());
+		m.addAttribute("PlantName", post.getPlantName());
+		m.addAttribute("Species", post.getSpecies());
+		m.addAttribute("NameOfUser", post.getNameOfUser());
+		m.addAttribute("Caption", post.getCaption());
+		m.addAttribute("PhotoUrl", post.getPhotoUrl());
+		System.out.print(post);
+
+		sqlDB.uploadPost(post);
+		return "uploadSuccess";
 	}
 
 	/* function httpGet(Url)
